3b2c0fd4f600fb623bda1f81ba27d4b51136f799
lerapodelyakina@mail.ru <lerapodelyakina@mail.ru>
2022-11-20 19:02:09 +0300

    3 лаба

--- Lab3/4.py
+++ Lab3/4.py
@@ -1,11 +1,11 @@
 n = int(input())
 for i in range(1, n+1):
     for p in range (n - i):
-        print(" ", end = ' ')
+        print(" ", end = '')
     for s in range(1, i):
-        print(s, end = ' ')
+        print(s, end = '')
     for i in range(i, 0, -1):
-        print(i, end = ' ')
+        print(i, end = '')
     for p in range(n - i):
-        print(" ", end=' ')
+        print(" ", end='')
     print("\n")
